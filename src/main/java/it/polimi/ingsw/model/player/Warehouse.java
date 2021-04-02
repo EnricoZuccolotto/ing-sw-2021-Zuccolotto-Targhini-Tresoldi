32d0b5ff90b2f2441f900642506b30d19a4a35fb
Enrico-Targhini <enrico.targhini@mail.polimi.it>
2021-04-02 11:18:45 +0200

    Pop Warehouse

--- src/main/java/it/polimi/ingsw/model/player/Warehouse.java
+++ src/main/java/it/polimi/ingsw/model/player/Warehouse.java
@@ -5,12 +5,16 @@ import it.polimi.ingsw.model.enums.Resources;
 public class Warehouse {
     private final Resources[] warehouse;
     private Resources[] var= {Resources.WHITE, Resources.WHITE, Resources.WHITE};
+    private int Resnum;
+    private Resources[] ResRow;
 
     public Warehouse() {
-        warehouse= new Resources[6];
+        warehouse = new Resources[6];
+        Resnum = 0;
+        ResRow = new Resources[3];
         int i;
-        for(i=0; i<6; i++){
-            warehouse[i]=Resources.WHITE;
+        for (i = 0; i < 6; i++) {
+            warehouse[i] = Resources.WHITE;
         }
     }
 
@@ -25,6 +29,7 @@ public class Warehouse {
                 if(warehouse[0]==Resources.WHITE){
                     warehouse[0]=resource;
                     var[0]=resource;
+                    Resnum+=Resnum;
                     return true;
                 }
                 else return false;  //riga piena
@@ -32,10 +37,12 @@ public class Warehouse {
                 if(warehouse[1]==Resources.WHITE){
                     warehouse[1]=resource;
                     var[1]=resource;
+                    Resnum+=Resnum;
                     return true;
                 }
                 else if(warehouse[1]==resource && warehouse[2]==Resources.WHITE){
                     warehouse[2]=resource;
+                    Resnum+=Resnum;
                     return true;
                 }
                 else return false; // riga piena o altre risorse nella riga
@@ -43,15 +50,18 @@ public class Warehouse {
                 if(warehouse[3]==Resources.WHITE) {
                     warehouse[3] = resource;
                     var[2]=resource;
+                    Resnum+=Resnum;
                     return true;
                 }
                 else if(warehouse[3]==resource){
                     if(warehouse[4]==Resources.WHITE){
                         warehouse[4]=resource;
+                        Resnum+=Resnum;
                         return true;
                     }
                     else if(warehouse[5]==Resources.WHITE){
                         warehouse[5]=resource;
+                        Resnum+=Resnum;
                         return true;
                     }
                 else return false;
@@ -102,12 +112,34 @@ public class Warehouse {
 
     /*private void checkConsistency() {
     }*/
-    public void printWarehouse(){
+    public String toString(){
         System.out.println(""+warehouse[0]);
         System.out.print(""+warehouse[1]);
         System.out.println("  "+warehouse[2]);
         System.out.print(""+warehouse[3]);
         System.out.print("  "+warehouse[4]);
         System.out.println("  "+warehouse[5]);
+        return null;
+    }
+
+    public boolean popResources(Resources resource){
+        int i=0;
+        while(var[i]!=resource){
+            i++;
+            if(i==3){
+                return false;
+            }
+        }
+        if(i==2){
+            i++;
+        }
+        while(warehouse[i+1]==resource){
+            i++;
+        }
+        warehouse[i]=Resources.WHITE;
+        return true;
+    }
+    public int Resourcesnumb(){
+        return Resnum;
     }
 }
